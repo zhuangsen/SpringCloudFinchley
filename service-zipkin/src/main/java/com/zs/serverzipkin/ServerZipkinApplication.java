b061f1654d9e0ab9bf06ac91b12c54914ef4b31c
zhuangsen <zhuangsen@live.com>
2018-11-01 16:55:08 +0800

    update

--- service-zipkin/src/main/java/com/zs/serverzipkin/ServerZipkinApplication.java
+++ service-zipkin/src/main/java/com/zs/serverzipkin/ServerZipkinApplication.java
@@ -2,9 +2,10 @@ package com.zs.serverzipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-//@EnableZipkinServer
+@EnableZipkinServer
 public class ServerZipkinApplication {
 
 	public static void main(String[] args) {
